61b260a06bde360dab7773d99a32d97f9052e5bc
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2020-11-11 21:51:10 -0600

    worked up to Models:number2 step before stopping

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,13 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
-    private String name;
+    @NotNull
+    @Length (min=1,max=200)
+    public String name;
 
     public int getId() {
         return id;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    private String location;
 
+    public Employer() {
+
+    }
+
+    public Employer(String aLocation) {
+        super();
+        this.location = aLocation;
+    }
+
+    //getters and setters
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
